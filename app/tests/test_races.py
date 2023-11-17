3f28c876bbef3827d3a493c3ea8dd2b2459c0a4b
KozielPiotr <41578872+KozielPiotr@users.noreply.github.com>
2023-11-17 09:28:06 +0100

    base race and character classes and modify character abilities method

---
+++ app/tests/test_races.py
@@ -0,0 +1,19 @@
+"""Tests for Race class"""
+
+from app.characters_classes_races.character import Character
+from app.characters_classes_races.races import Human
+
+
+class TestRaces:
+    def test_modify_abilities(self):
+        """Character abilities are set to 0 by default"""
+
+        character = Character()
+        race = Human()
+        character = race.modify_abilities(character)
+        assert character.strength == 2
+        assert character.condition == 2
+        assert character.dexterity == 2
+        assert character.intelligence == 2
+        assert character.wisdom == 2
+        assert character.charisma == 2
